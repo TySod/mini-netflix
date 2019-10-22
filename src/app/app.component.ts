4efe1201066d98a1a24316bd0d74dc9aa277db5e
Taiwo <oyeniyi4real@gmail.com>
2019-10-22 07:05:01 +0100

    Basic Components

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  template:`
+<div>
+  <nav-bar> </nav-bar>
+  <h2>Minflixapp </h2>
+</div>
+  `,
+})
+export class AppComponent {
+  title = 'mini-netflix';
+}
